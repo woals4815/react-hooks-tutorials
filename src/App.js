b2278c48184c8d7c306218ec98cf67a90e3b5676
Jayden JaeMin Jeong <71315300+woals4815@users.noreply.github.com>
2021-04-15 23:45:35 +0900

    Learn useEffect

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
+import { useState } from "react";
 import "./App.css";
 import Counter from "./Counter";
+import Info from "./Info";
 
 function App() {
-  return <Counter />;
+  const [visible, setVisible] = useState(false);
+  return (
+    <div>
+      <button
+        onClick={() => {
+          setVisible(!visible);
+        }}
+      >
+        {visible ? "숨기기" : "보이기"}
+      </button>
+      <hr />
+      {visible && <Info />}
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Info.js
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from "react";
+
+const Info = () => {
+  const [name, setName] = useState("");
+  const [nickname, setNickname] = useState("");
+  useEffect(() => {
+    console.log("렌더링이 완료됐습니다.");
+    console.log({
+      name,
+      nickname,
+    });
+    return () => {
+      console.log("clean up");
+    };
+  }, [name]);
+  const onChangeName = (e) => {
+    setName(e.target.value);
+  };
+  const onChangeNickname = (e) => {
+    setNickname(e.target.value);
+  };
+  return (
+    <div>
+      <div>
+        <input value={name} onChange={onChangeName} />
+        <input value={nickname} onChange={onChangeNickname} />
+      </div>
+      <div>
+        <div>
+          <b>이름:</b> {name}
+        </div>
+        <div>
+          <b>닉네임: </b> {nickname}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Info;
