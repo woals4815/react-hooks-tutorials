92e19b35ccf4016d79fe6f62d0f27a85c63c3b8d
Jayden JaeMin Jeong <71315300+woals4815@users.noreply.github.com>
2021-04-16 00:18:58 +0900

    Learn custom hooks

--- src/App.js
+++ src/App.js
@@ -5,19 +5,7 @@ import Info from "./Info";
 
 function App() {
   const [visible, setVisible] = useState(false);
-  return (
-    <div>
-      <button
-        onClick={() => {
-          setVisible(!visible);
-        }}
-      >
-        {visible ? "숨기기" : "보이기"}
-      </button>
-      <hr />
-      {visible && <Info />}
-    </div>
-  );
+  return <Info />;
 }
 
 export default App;
--- src/Info.js
+++ src/Info.js
@@ -1,29 +1,17 @@
 import React, { useEffect, useState } from "react";
+import useInputs from "./useInputs";
 
 const Info = () => {
-  const [name, setName] = useState("");
-  const [nickname, setNickname] = useState("");
-  useEffect(() => {
-    console.log("렌더링이 완료됐습니다.");
-    console.log({
-      name,
-      nickname,
-    });
-    return () => {
-      console.log("clean up");
-    };
-  }, [name]);
-  const onChangeName = (e) => {
-    setName(e.target.value);
-  };
-  const onChangeNickname = (e) => {
-    setNickname(e.target.value);
-  };
+  const [state, onChange] = useInputs({
+    name: "",
+    nickname: "",
+  });
+  const { name, nickname } = state;
   return (
     <div>
       <div>
-        <input value={name} onChange={onChangeName} />
-        <input value={nickname} onChange={onChangeNickname} />
+        <input name="name" value={name} onChange={onChange} />
+        <input name="nickname" value={nickname} onChange={onChange} />
       </div>
       <div>
         <div>
